f3f393030e6696ca8cf8432544080f683072dca4
Dark Tornado <dt3141592@naver.com>
2022-10-01 08:13:48 +0900

    모둘 자동 적용기 구현

--- kaling.js
+++ kaling.js
@@ -58,9 +58,13 @@ Based on Delta's kaling.js
         sender.findRoom(room);
         sender.send();
     };
-    
+
     Kakao.prototype.getVersion = function() {
         return VERSION;
+    };
+
+    Kakao.prototype.applyDownloader = function(ctx) {
+        createModuleDownloader(ctx);
     };
 
     /* Kakao Web Login */
@@ -202,6 +206,212 @@ Based on Delta's kaling.js
             .method(org.jsoup.Connection.Method.POST)
             .execute();
     };
+
+
+    /* Module Downloader */
+    const GithubURL = 'https://raw.githubusercontent.com/DarkTornado/KakaoLink.js/main/';
+    var ctx;
+
+    function createModuleDownloader(_ctx) {
+        ctx = _ctx;
+        createUI();
+    }
+
+    function createUI() {
+        var layout0 = new android.widget.LinearLayout(ctx);
+        layout0.setOrientation(1);
+        var title = new android.widget.Toolbar(ctx);
+        title.setTitle('kaling.js 모듈 다운로더');
+        title.setTitleTextColor(android.graphics.Color.WHITE);
+        title.setBackgroundColor(android.graphics.Color.BLACK);
+        var margin = new android.widget.LinearLayout.LayoutParams(-1, -2);
+        margin.setMargins(0, 0, 0, dip2px(ctx, 10));
+        title.setLayoutParams(margin);
+        title.setElevation(dip2px(ctx, 3));
+        layout0.addView(title);
+
+        var layout = new android.widget.LinearLayout(ctx);
+        layout.setOrientation(1);
+
+        var txt = new android.widget.TextView(ctx);
+        txt.setText(' 개발자의 깃허브에서 카카오링크를 다운로드 받아서 자동으로 적용하는 기능입니다.\n' +
+            ' "채팅 자동응답 봇"에서 사용시 모듈이 바로 적용되고, "메신저봇"에서 사용시 "/내장메모리/Download/" 폴더에 저장됩니다.\n\n' +
+            '현재 카링 모듈 버전 : ' + VERSION);
+        txt.setTextSize(18);
+        layout.addView(txt);
+
+        var check = new android.widget.Button(ctx);
+        check.setText('최신 버전 확인');
+        check.setOnClickListener(new android.view.View.OnClickListener() {
+            onClick: function(v) {
+                var version = getNewestVersion();
+                if (version == null) toast('최신버전 확인 실패');
+                else showDialog('버전 정보', '현재 버전 : ' + VERSION + '\n최신 버전 : ' + version);
+            }
+        });
+        layout.addView(check);
+        var kaling = new android.widget.Button(ctx);
+        kaling.setText('kaling.js 다운로드');
+        kaling.setTransformationMethod(null);
+        kaling.setOnClickListener(new android.view.View.OnClickListener() {
+            onClick: function(v) {
+                checkBotType('kaling');
+            }
+        });
+        layout.addView(kaling);
+        var crypto = new android.widget.Button(ctx);
+        crypto.setText('crypto.js 다운로드');
+        crypto.setTransformationMethod(null);
+        crypto.setOnClickListener(new android.view.View.OnClickListener() {
+            onClick: function(v) {
+                checkBotType('crypto');
+            }
+        });
+        layout.addView(crypto);
+
+        var maker = new android.widget.TextView(ctx);
+        maker.setText('\n© 2021-2022 Dark Tornado, All rights reserved.\n');
+        maker.setTextSize(12);
+        maker.setGravity(android.view.Gravity.CENTER);
+        layout.addView(maker);
+
+        var pad = dip2px(ctx, 16);
+        layout.setPadding(pad, pad, pad, pad);
+        var scroll = new android.widget.ScrollView(ctx);
+        scroll.addView(layout);
+        layout0.addView(scroll);
+        ctx.setContentView(layout0);
+        android.os.StrictMode.enableDefaults();
+    }
+
+    function getNewestVersion() {
+        var url = GithubURL + 'kaling.js';
+        var data = getWebText(url);
+        Log.i(url)
+        if (!data.includes('const VERSION = ')) return null;
+        data = data.split('\n');
+        for (var n = 0; n < data.length; n++) {
+            if (data[n].trim().startsWith('const VERSION = ')) {
+                var version = data[n].split('const VERSION = \'')[1].split('\';')[0];
+                return version;
+            }
+        }
+        return null;
+    }
+
+    function checkBotType(fileName) {
+        var type = getBotType();
+        if (type == '채팅 자동응답 봇') {
+            prepareDownload(fileName, 'ChatBot/module');
+        } else if (type == '메신저봇') {
+            prepareDownload(fileName, 'Download');
+        } else {
+            showDialog('기능 사용 불가능', '현재 사용중이신 봇 구동 앱이 무엇인지 식벽하지 못했어요.');
+        }
+    }
+
+    function prepareDownload(fileName, dir) {
+        var sdcard = android.os.Environment.getExternalStorageDirectory().getAbsolutePath();
+        var file = new java.io.File(sdcard + '/' + dir + '/' + fileName + '.js');
+        if (file.exists()) {
+            alertFileExists(fileName, file);
+        } else {
+            download(fileName, file);
+        }
+    }
+
+    function alertFileExists(fileName, file) {
+        var dialog = new android.app.AlertDialog.Builder(ctx);
+        dialog.setTitle('파일이 이미 있습니다');
+        dialog.setMessage('기존에 있던 모듈 파일을 덮어씌우시겠습니까?');
+        dialog.setNegativeButton('아니요', null);
+        dialog.setPositiveButton('네', new android.content.DialogInterface.OnClickListener({
+            onClick: function(v) {
+                download(fileName, file);
+            }
+        }));
+        dialog.show();
+    }
+
+    function download(fileName, file) {
+        var downloaded = copyFromWeb(GithubURL + fileName + '.js', file);
+        if (downloaded) showDialog('모듈 다운로드 완료', '모듈 파일을 다운로드했어요\n위치: ' + file);
+        else showDialog('모듈 다운로드 실패', '모듈 파일을 다운로드하지 못했어요 :(');
+    }
+
+    function getWebText(url) {
+        try {
+            var url = new java.net.URL(url);
+            var con = url.openConnection();
+            if (con != null) {
+                con.setConnectTimeout(5000);
+                con.setUseCaches(false);
+                var isr = new java.io.InputStreamReader(con.getInputStream());
+                var br = new java.io.BufferedReader(isr);
+                var str = br.readLine();
+                var line = '';
+                while ((line = br.readLine()) != null) {
+                    str += '\n' + line;
+                }
+                isr.close();
+                br.close();
+                con.disconnect();
+            }
+            return str + '';
+        } catch (e) {
+            Log.e(e, true);
+        }
+        return '';
+    }
+
+    function copyFromWeb(url, file) {
+        try {
+            var url = new java.net.URL(url);
+            var con = url.openConnection();
+            if (con != null) {
+                con.setConnectTimeout(5000);
+                con.setUseCaches(false);
+                var bis = new java.io.BufferedInputStream(con.getInputStream());
+                var fos = new java.io.FileOutputStream(file);
+                var bos = new java.io.BufferedOutputStream(fos);
+                var buf;
+                while ((buf = bis.read()) != -1) {
+                    bos.write(buf);
+                }
+                bis.close();
+                bos.close();
+                con.disconnect();
+                fos.close();
+            }
+            return true;
+        } catch (e) {
+            Log.e(e, true);
+        }
+        return false;
+    }
+
+    function showDialog(title, msg) {
+        var dialog = new android.app.AlertDialog.Builder(ctx);
+        dialog.setTitle(title);
+        dialog.setMessage(msg);
+        dialog.setNegativeButton('닫기', null);
+        dialog.show();
+    }
+
+    function toast(msg) {
+        android.widget.Toast.makeText(ctx, msg, android.widget.Toast.LENGTH_LONG).show();
+    }
+
+    function dip2px(ctx, dips) {
+        return Math.ceil(dips * ctx.getResources().getDisplayMetrics().density);
+    }
+
+    function getBotType() {
+        if (typeof com.darktornado.chatbot.BuildConfig == 'function') return '채팅 자동응답 봇';
+        if (typeof com.xfl.msgbot.BuildConfig == 'function') return '메신저봇';
+        return '알 수 없음';
+    }
+   
 
     /* Legacy Module Compatible */
     Kakao.Kakao = function() {
